f89550a26ded9dcf558bacbd0b4b35f4882cd22e
Muhammad Ilyas <muhammad.ilyas@jubelio.com>
2022-12-23 19:36:15 +0700

    Initial Commit

---
+++ CovidApps/CovidAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidAppsApp.swift
+//  CovidApps
+//
+//  Created by Muhammad Ilyas on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CovidAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
